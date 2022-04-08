ab85bce70570c2bb6b718a8a2869c6afef06d204
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-04-08 05:33:07 +0430

    Initial Commit

---
+++ Mood/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mood
+//
+//  Created by Ali Aghamirbabaei on 4/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mood/MoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodApp.swift
+//  Mood
+//
+//  Created by Ali Aghamirbabaei on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct MoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
